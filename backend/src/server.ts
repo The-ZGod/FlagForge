fbe78af943e7e0dcb10047d392be0d312d0645f5
Arihant <arihantalagoudar@gmail.com>
2026-09-16 11:25:22 +0530

    refactor(api): separate express app from server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,6 @@
-import express from "express";
-
-const app = express();
+import app from "./app.js";
 
 const PORT = 3000;
-
-app.get("/", (_req, res) => {
-    res.json({
-        message: "FlagForge API is running",
-    });
-});
 
 app.listen(PORT, () => {
     console.log(`FlagForge API running on port ${PORT}`);
